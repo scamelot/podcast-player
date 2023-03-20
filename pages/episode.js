fe2e9df19dd0d4001f5f949591e3cba47868b0c6
Steve Scammon <stevescammon@me.com>
2023-03-20 10:16:30 -0400

    tailwind uplift

--- pages/episode.js
+++ pages/episode.js
@@ -7,12 +7,17 @@ function Episode({ episode }) {
   return (
     <div>
     { episode ? (
-    <div>
-      <h2>{episode.title}</h2>
-      <img src={episode.itunes.image} width='100px' height='100px'></img>
-      <audio src={episode.enclosure.url} preload="none" controls></audio>
-      <div dangerouslySetInnerHTML={{ __html: episode.content }}></div>
-    </div> ) : (<h2>Loading...</h2>)}
+     <div className="bg-white shadow-md rounded-lg p-6 my-4 w-full flex flex-col items-center">
+     <h2 className="text-xl font-semibold text-gray-700 mb-4">{episode.title}</h2>
+     <img src={episode.itunes.image} className="w-64 h-64 my-2 object-cover rounded-lg shadow-md"></img>
+     <audio
+       src={episode.enclosure.url}
+       preload="none"
+       controls
+       className="w-full mb-4"
+     />
+     <div className="text-gray-700" dangerouslySetInnerHTML={{__html: episode.content}} />
+   </div> ) : (<h2 className="text-gray-700">Loading...</h2>)}
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -26,12 +26,12 @@ export default function Home() {
       { currentEpisode.enclosure ? (
       <div>
       <Episode episode={currentEpisode} />
-      <div>
-        <a href={`/?episodeIndex=${previousEpisodeIndex}`}>Previous</a>
-        <a href={`/?episodeIndex=${nextEpisodeIndex}`}>Next</a>
+      <div className="flex justify-between mt-4">
+        <a className="bg-gray-500 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-gray-700 active:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 mr-2" href={`/?episodeIndex=${previousEpisodeIndex}`}>Previous</a>
+        <a className="bg-blue-500 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-blue-700 active:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 mr-2"href={`/?episodeIndex=${nextEpisodeIndex}`}>Next</a>
       </div>
       </div>
-      ) : (
+      ) : ( 
   <h2>Loading...</h2>
       )}
     </div>
